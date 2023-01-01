ab50ea22ed699e6431c6f1059c7bf5d1e17eb3c9
Aidenzich <aidenzich0221@gmail.com>
2023-01-01 21:42:08 +0800

    Add multi-thread example and update README

--- README.md
+++ README.md
@@ -15,3 +15,19 @@ If you are new to RabbitMQ, this repo should provide you with the knowledge and 
 | Queue    | A collection of entities that are maintained in a sequence and can be modified by the addition of entities at one end of the sequence and the removal of entities from the other end of the sequence.  |
 | Exchange |  Exchange is used as a routing mediator, to receive messages from producers and push them to message queues according to rules provided by the RabbitMQ exchange type.   |
 | Message  | A message can include any kind of information. |
+
+### Durability when restarting the container
+When RabbitMQ quits or crashes it will forget the queues and messages unless **you tell it not to**. Two things are required to make sure that messages aren't lost: **mark both the queue and messages as durable**.
+
+### Get vs Consume
+There are two different AMQP RPC commands for retrieving messages from a queue in RabbitMQ:
+| Commands | Description |
+|-|-|
+| `Basic.Get` | It's a polling model, meaning it must send a new request each time it wants to receive a message |
+| `Basic.Consume` | It's a push model. It sends messages asynchronously to your consumer as they become available. |
+
+### Types of Messages
+| Type | |
+|-|-|
+| `Ready` | When a message is waiting to be processed |
+| `Unacked` | While a consumer is working on the messages, they get the status unacked, which means that the consumer has promised to process them but has not acknowledged them yet. When the consumer crashes, the messages will be delivered again |
--- main.py
+++ main.py
@@ -1,33 +1,34 @@
 #%%
 import pika
 
-#%%
+# Producer
 connection = pika.BlockingConnection()
 
 channel = connection.channel()
 # If exchange does not exist, create it.
-channel.exchange_declare(exchange="fever", exchange_type="direct")
+channel.exchange_declare(exchange="test", exchange_type="direct")
 
 # If queue does not exist, create it.
-channel.queue_declare(queue="title")
+channel.queue_declare(queue="test_q")
 
 # Bind the queue to the exchange with the given routing key.
-channel.queue_bind(queue="title", exchange="fever", routing_key="title")
+channel.queue_bind(queue="test_q", exchange="test", routing_key="test_q")
 
 # Publish to the channel with the given exchange, routing key and body.
-for i in range(15):
-    channel.basic_publish(exchange="fever", routing_key="title", body=f"Title{i}")
+for i in range(150):
+    channel.basic_publish(exchange="test", routing_key="test_q", body=f"Title{i}")
 
 
 connection.close()
 
 
 # %%
+# Get
 connection = pika.BlockingConnection()
 channel = connection.channel()
 
 while True:
-    method_frame, header_frame, body = channel.basic_get("title")
+    method_frame, header_frame, body = channel.basic_get("test_q")
     if method_frame:
         print(method_frame, header_frame, body)
         channel.basic_ack(method_frame.delivery_tag)
@@ -36,4 +37,26 @@ while True:
         break
 
 connection.close()
+# %%
+import pika
+
+# Consumer
+connection = pika.BlockingConnection()
+channel = connection.channel()
+
+for method_frame, properties, body in channel.consume("test_q"):
+    # Display the message parts and acknowledge the message
+    print(method_frame, properties, body)
+    channel.basic_ack(method_frame.delivery_tag)
+
+    # # Escape out of the loop after 10 messages
+    # if method_frame.delivery_tag == 10:
+    #     break
+
+
+# Cancel the consumer and return any pending messages
+requeued_messages = channel.cancel()
+print("Requeued %i messages" % requeued_messages)
+connection.close()
+
 # %%
